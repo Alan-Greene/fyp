2384b6e5c006c21a3b23e1970844c8f3bb80d55e
Alan-Greene <x00124937@mytudublin.ie>
2022-04-14 16:11:04 +0100

    detecting bugs

--- server/services/sms.js
+++ server/services/sms.js
@@ -7,10 +7,8 @@ AWS.config.update({ region: 'us-east-1' });
 function sendSms(url) {
     // Create publish parameters
 
-    JSON.stringify(url);
-
     var params = {
-        Message: 'http://ec2-18-232-152-228.compute-1.amazonaws.com/url/individual/' + '' + url, /* required */
+        Message: `http://ec2-18-232-152-228.compute-1.amazonaws.com/url/individual/${url}`, /* required */
         PhoneNumber: '+353896154318',
     };
 
