98c1193921266c54af35ad378bf842d6ed5967a7
Alan-Greene <x00124937@mytudublin.ie>
2022-04-15 12:21:21 +0100

    debugging SMS

--- server/services/sms.js
+++ server/services/sms.js
@@ -1,13 +1,13 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set region
-AWS.config.update({ region: 'us-east-1a' });
+AWS.config.update({ region: 'us-east-1' });
 
 //take in number variable here when in production
 function sendSms(url) {
     // Create publish parameters
     var params = {
-        Message: 'ec2-54-159-244-208.compute-1.amazonaws.com/url/individual/' + '' + url, /* required */
+        Message: 'ec2-54-159-244-208.compute-1.sns.amazonaws.com/url/individual/' + '' + url, /* required */
         PhoneNumber: '+353896154318',
     };
 
