d9649672eb2d439b25afdfc2a287e4805ce17cbe
Alan-Greene <x00124937@mytudublin.ie>
2022-04-19 01:33:09 +0100

    port allocation fix

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const vonage = new Vonage({
 
 
 // Specify Host and port
-const HOST = 'ec2-52-206-51-176.compute-1.amazonaws.com';
+const HOST = '52.206.51.176';
 const PORT = 80;
 
 // app is a new instance of express (the web app framework)
