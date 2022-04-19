1ca449a67242e218f04d548dd25fa159f8171ef5
Alan-Greene <x00124937@mytudublin.ie>
2022-04-19 23:34:56 +0100

    how did this happen

--- client/server.js
+++ client/server.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express();
 
 // Defile the server host IP and port
-const HOST = 'ec2-18-204-71-236.compute-1.amazonaws.com';
+const HOST = 'ec2-54-234-7-248.compute-1.amazonaws.com';
 // if port defined in ENV, use that value, otherwise 3000
 const PORT = 3000;
 
