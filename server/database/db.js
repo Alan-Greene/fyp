077671751e7e59ea4d2a12eee23efaabeb56ad16
Alan-Greene <x00124937@mytudublin.ie>
2022-04-20 00:13:06 +0100

    db

--- server/database/db.js
+++ server/database/db.js
@@ -2,13 +2,14 @@
 const Database = require('better-sqlite3');
 const dbConn = new Database('./database/patientdb.db', { verbose: console.log });
 
-// class patientDatabase {
-//     constructor(path) {
-//         this.conn = new Database(path, { verbose: console.log });
-//     }
-// }
 
 // export the database connection object
 module.exports = {
     dbConn
 };
+
+class patientDatabase {
+    constructor(path) {
+        this.conn = new Database(path, { verbose: console.log });
+    }
+}
